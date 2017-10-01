bada236a36f0943a2cc5697cca476fa970017107
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-10-01 17:07:34 -0300

    foi implementado para piscar 3 leds

--- led/led.ino
+++ led/led.ino
@@ -1,20 +1,30 @@
 #define LED_VERMELHO 2
+#define LED_VERDE 3
+#define LED_AMARELO 4
+
+#define UM_SEGUNDO 1000
+#define MEIO_SEGUNDO 500
 
 void setup() {
+  Serial.begin(9600);
   iniciarPortas();
 }
 
 void loop() {
   piscarLed(LED_VERMELHO);
+  piscarLed(LED_VERDE);
+  piscarLed(LED_AMARELO);
 }
 
 void iniciarPortas() {
   pinMode(LED_VERMELHO, OUTPUT);
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(LED_AMARELO, OUTPUT);
 }
 
 void piscarLed(int porta) {
  digitalWrite(porta, HIGH);
- delay(1000);
+ delay(UM_SEGUNDO);
  digitalWrite(porta, LOW);   
- delay(500);
+ delay(MEIO_SEGUNDO);
 }
